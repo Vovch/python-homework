89ea60242f1ad88fea25d815acc64b91e88ec455
Vladimir Mironov <vladimir_mironov2@epam.com>
2022-12-01 21:41:21 +0300

    Update distribution package and version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tagcounter',
-    version='1.0',
+    version='1.1',
     author='Vladimir_Mironov2',
     packages=['tagcounter'],
     description='Description',
